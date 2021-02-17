6590e1c7c303e4587fc7a14eadd526edeac83113
tigasgon1999 <tigasgon1999@gmail.com>
2021-02-17 11:34:42 +0000

    Added function to create directories for the data.

--- data_loader.py
+++ data_loader.py
@@ -37,10 +37,43 @@ def create_csv():
     df = pd.DataFrame({"image_id": imgs[:, 0], "folder": folders, "label": imgs[:, 1]}) 
     df.to_csv('catdogs.csv', index=False) # Save DataFrame as a csv file
 
+
+def create_dirs(root_dir, splits, classes):
+    # Create root directory, if it does not exist
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+    
+    for split in splits:
+        split_name = 'split_'+str(split)
+        # Create split directory, if it does not exist
+        if not os.path.exists(root_dir+split_name):
+            os.makedirs(root_dir+split_name)
+        
+        # Create train directory for the current split, if it does not exist
+        if not os.path.exists(root_dir+split_name+'/train/'):
+            os.makedirs(root_dir+split_name+'/train/')
+        # Create validation directory for the current split, if it does not exist
+        if not os.path.exists(root_dir+split_name+'/train/'):
+            os.makedirs(root_dir+split_name+'/train/')
+
+        for c in classes:
+            # Create class directories for the current split, if it does not exist
+            if not os.path.exists(root_dir+split_name+'/train/'+c+'/'):
+                os.makedirs(root_dir+split_name+'/train/'+c+'/')
+            if not os.path.exists(root_dir+split_name+'/val/'+c+'/'):
+                os.makedirs(root_dir+split_name+'/val/'+c+'/')
+
+
+
 def replace_files(dataset):
     root_dir = './data/'
 
-    splits = {1:['A','B','C'], 2:['B','C','A'], 3: ['A', 'C', 'D']}
+    splits = {1:['A','B','C'], 2:['B','C','A'], 3: ['A', 'C', 'B']}
+
+    classes = ['cat', 'dog']
+
+    # Create directories, if necessary
+    create_dirs(root_dir, splits, classes)
 
     for split in splits:
         print(f"On split {split}")
@@ -50,17 +83,16 @@ def replace_files(dataset):
             fold = list(dataset['folder'])[line]
 
             split_name = 'split_'+str(split)
+
             dog_or_cat = old_file_name.split('_')[0]            
 
             if fold == splits[split][0] or fold == splits[split][1]:
-                new_path = root_dir+split_name+'/train'+'/'+dog_or_cat+'/'+old_file_name
-                copyfile(old_path, new_path)
-            
+                new_path = root_dir+split_name+'/train/'+dog_or_cat+'/'+old_file_name
+                copyfile(old_path, new_path)         
             else:
-                new_path = root_dir+split_name+'/val'+'/'+dog_or_cat+'/'+old_file_name
+                new_path = root_dir+split_name+'/val/'+dog_or_cat+'/'+old_file_name
                 copyfile(old_path, new_path)
 
 if __name__ == '__main__':
     dataset = pd.read_csv('catdogs.csv')
     replace_files(dataset)
-
