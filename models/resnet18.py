494a215c8557d74364604dfa2767ed1b38c6ff31
tigasgon1999 <tigasgon1999@gmail.com>
2021-02-17 15:45:15 +0000

    Fixed cross-validation loop.

--- models/resnet18.py
+++ models/resnet18.py
@@ -14,6 +14,8 @@ import os
 import copy 
 from tqdm import tqdm
 
+INPUT_SIZE = 224
+
 def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=25, is_inception=False):
     since = time.time()
 
@@ -120,15 +122,15 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         set_parameter_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
+        input_size = INPUT_SIZE
 
     else:
         print("Invalid model name, exiting...")
         exit()
 
-    return model_ft, input_size
+    return model_ft
 
-def get_K_fold_data(data_dir, bs):
+def get_K_fold_data(data_dir, bs, input_size):
     # Data augmentation and normalization for training
     # Just normalization for validation
     data_transforms = {
@@ -174,45 +176,48 @@ if __name__ == "__main__":
     # when True we only update the reshaped layer params
     feature_extract = True
 
-    # Initialize the model for this run
-    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
 
     print("Initializing Datasets and Dataloaders...")
     # Get cross-validation data
-    data_loaders_dict = get_K_fold_data(data_dir, bs= batch_size)
+    data_loaders_dict = get_K_fold_data(data_dir, bs=batch_size, input_size=INPUT_SIZE)
     
     # Detect if we have a GPU available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    # Send the model to GPU
-    model_ft = model_ft.to(device)
-
-    # Initialize optimizer
-    params_to_update = model_ft.parameters()
-
-    if feature_extract:
-        params_to_update = []
-        for name, param in model_ft.named_parameters():
-            if param.requires_grad == True:
-                params_to_update.append(param)
-
-    else:
-        for name, param in model_ft.named_parameters():
-            if param.requires_grad == True:
-                pass
-
-    # Define optimizer
-    optimizer_ft = optim.SGD(params_to_update, lr=0.01, momentum=0.9)
-
-    # Setup the loss function
-    criterion =  nn.CrossEntropyLoss()
-
-    # Train 3 full models
-    # Define variables to store values
-    training_output = {}
 
     for loader in data_loaders_dict:
         print(f"\n ## On split No. {loader} ##\n")
+
+        # Initialize the model for this run
+        model_ft = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+        # Send the model to GPU
+        model_ft = model_ft.to(device)
+
+        # Initialize optimizer
+        params_to_update = model_ft.parameters()
+
+        if feature_extract:
+            params_to_update = []
+            for name, param in model_ft.named_parameters():
+                if param.requires_grad == True:
+                    params_to_update.append(param)
+
+        else:
+            for name, param in model_ft.named_parameters():
+                if param.requires_grad == True:
+                    pass
+
+        # Define optimizer
+        optimizer_ft = optim.SGD(params_to_update, lr=0.01, momentum=0.9)
+
+        # Setup the loss function
+        criterion =  nn.CrossEntropyLoss()
+
+        # Train 3 full models
+        # Define variables to store values
+        training_output = {}
+
         # Train model
         model_ft, val_acc_history, val_loss_history, train_acc_history, train_loss_history, best_acc, time_elapsed = train_model(
             model_ft, data_loaders_dict[loader], criterion, optimizer_ft, device, num_epochs=num_epochs, is_inception=(model_name=="inception"))
